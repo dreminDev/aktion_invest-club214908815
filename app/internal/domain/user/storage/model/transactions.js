339e1f1498bf743633aa446e21759c065b788751
Максим <126281157+dreminDev@users.noreply.github.com>
2023-11-26 01:18:38 +0300

    Update transactions.js

--- app/internal/domain/user/storage/model/transactions.js
+++ app/internal/domain/user/storage/model/transactions.js
@@ -1,37 +1,37 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const TransactionsSchema = new Schema({
-  type: {
-    type: "string",
-    required: true,
-  },
-  senderId: {
-    type: "number",
-  }, 
-  recipientId: {
-    type: "number"
-  },
-  currency: {
-    type: "string",
-    required: true
-  },
-  amount: {
-    type: "number",
-    required: true, 
-    min: 1
-  },
-  metaData: {
-    type: "object",
-  },
-  createdAt: {
-    type: "number",
-    default: Date.now(),
-    required: true, 
-  }
-})
+    type: {
+        type: "string",
+        required: true,
+    },
+    senderId: {
+        type: "number",
+    },
+    recipientId: {
+        type: "number",
+    },
+    currency: {
+        type: "string",
+        required: true,
+    },
+    amount: {
+        type: "number",
+        required: true,
+        min: 1,
+    },
+    metaData: {
+        type: "object",
+    },
+    createdAt: {
+        type: "number",
+        default: Date.now(),
+        required: true,
+    },
+});
 
 const Transactions = new model("transactions", TransactionsSchema);
 
 module.exports = {
-  Transactions,
+    Transactions,
 };
